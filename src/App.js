810537d05122713e48c177792c5691ecaa476a93
Ignacio Francos Koch <67810079+nachokoch@users.noreply.github.com>
2021-05-24 17:02:35 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+import React, { useState, useEffect } from "react";
+import Frase from "./Components/Frase";
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size 0.8s ease;
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
+`;
+
+function App() {
+  const [frase, guardarFrase] = useState({});
+
+  const consultarAPI = async () => {
+    const api = await fetch(
+      "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
+    const frase = await api.json();
+    guardarFrase(frase[0]);
+  };
+
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
+  return (
+    <Contenedor>
+      <Frase frase={frase} />
+      <Boton onClick={consultarAPI}>Obtener Frase</Boton>
+    </Contenedor>
+  );
+}
+
+export default App;
